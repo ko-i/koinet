13175133a888af9cf9d8f2bf975f1cfb81f4fd19
Akson <Akson.L@outlook.com>
2020-12-22 21:43:06 +0800

    quick update

--- src/app/tju/tju.module.ts
+++ src/app/tju/tju.module.ts
@@ -5,11 +5,15 @@ import { TjuIndexComponent } from './tju-index/tju-index.component';
 import { HighMathExamComponent } from './high-math-exam/high-math-exam.component';
 import { TjuRoutingModule } from './tju-routing.module';
 
+import { NzCardModule } from 'ng-zorro-antd/card';
+
 @NgModule({
   declarations: [TjuComponent, TjuIndexComponent, HighMathExamComponent],
   imports: [
     CommonModule,
     TjuRoutingModule,
+
+    NzCardModule
   ]
 })
 export class TjuModule { }
